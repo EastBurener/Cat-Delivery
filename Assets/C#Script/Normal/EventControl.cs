69de0f6680874f81406cd669666a8c77a958fff0
EastBurener <y3190824735@163.com>
2025-04-29 20:49:29 +0800

    feat：文件结构调整，可破碎对象

---
+++ Assets/C#Script/Normal/EventControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventControl : MonoBehaviour
+{
+    public GameObject bloodUI;
+
+    void Start()
+    {
+        bloodUI.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
